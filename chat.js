5ff4414125a25c40b11b4eea85731b53aa8c957c
IlinykhT <IlinykhTa999@gmail.com>
2018-10-03 13:38:29 +0700

    adding files

---
+++ chat.js
@@ -0,0 +1,64 @@
+var nickInput = document.getElementById('nickInput');
+var messageInput = document.getElementById('messageInput');
+var chatArea = document.getElementById('chatArea');
+var sendButton = document.getElementById('sendButton');
+
+var	chatHistory;
+
+if(localStorage.getItem("chat")) {
+	
+	chatHistory = JSON.parse(localStorage.getItem("chat"));
+	var template = "";
+	for (i = 0; i < chatHistory.length; i++) {
+		template += "<div><strong>" + chatHistory[i].nick + ": </strong>" + chatHistory[i].mess+"</div>";	
+	}
+	chatArea.innerHTML = template;
+	
+} else {
+	
+	chatHistory = JSON.stringify([]);
+	localStorage.setItem("chat", chatHistory);
+	
+}
+
+sendButton.addEventListener('click', function() {
+	
+	if (document.getElementById('nickInput').value == ""){
+		alert("please enter nickname");
+		return;
+	}
+	if (document.getElementById('messageInput').value == ""){
+		alert("please enter message");
+		return;
+	}
+
+	var message={
+		nick: nickInput.value,
+		mess: messageInput.value
+	};
+
+	nickInput.Value = "";
+	messageInput.value = "";
+	
+	chatHistory.push(message);
+    localStorage.setItem("chat", JSON.stringify(chatHistory));
+	
+	var template="<div><strong>" + message.nick + ": </strong>" + message.mess+ "</div>";
+	
+    chatArea.innerHTML += template;
+	chatArea.scrollTop=chatArea.scrollHeight;
+	
+});
+
+clearButton.addEventListener('click', function() {
+	localStorage.removeItem("chat");
+	chatArea.innerHTML="";	
+	chatHistory.clear();
+});
+
+
+function check() {
+  var nick = document.getElementById('nickInput'),
+      message = document.getElementById('messageInput');
+  document.getElementById('sendButton').disabled = nick.value && message.value ? false : "disabled";
+}
